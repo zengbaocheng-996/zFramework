85b95d8a3be8b55ece2e050adb1dbd0a63d5ec66
zengbaocheng <1154375864@qq.com>
2024-08-05 21:32:32 +0800

    update

--- Framework/Lua/scene/sceneManager.lua
+++ Framework/Lua/scene/sceneManager.lua
@@ -11,8 +11,8 @@ end
 function sceneManager.LoadScene(nextSceneName)
     -- // var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("MyObject");
     -- // Instantiate(prefab);
-    assetManger.loadedAssetBundle.LoadAsset(nextSceneName,function()SceneManager.LoadScene(nextSceneName)end);
-
+    -- assetManger.loadedAssetBundle.LoadAsset(nextSceneName,function()SceneManager.LoadScene(nextSceneName)end);
+    SceneManager.LoadScene(nextSceneName)
 end
 
 return sceneManager
