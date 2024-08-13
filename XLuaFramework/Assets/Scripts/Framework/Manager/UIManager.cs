4ff7dd6ab32b14ecb4a9bd72c5cee3e10fceb958
zengbaocheng <1154375864@qq.com>
2024-08-13 14:30:15 +0800

    update

--- XLuaFramework/Assets/Scripts/Framework/Manager/UIManager.cs
+++ XLuaFramework/Assets/Scripts/Framework/Manager/UIManager.cs
@@ -1,13 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     //缓存UI
     Dictionary<string, GameObject> m_UI = new Dictionary<string, GameObject>();
+    //ui分组
+    Dictionary<string, Transform> m_UIGroups = new Dictionary<string, Transform>();
+    private Transform m_UIParent;
 
-    public void OpenUI(string uiName, string luaName)
+    private void Awake()
+    {
+        m_UIParent = this.transform.parent.Find("UI");
+    }
+
+    public void SetUIGroup(List<string> group)
+    {
+        for (int i = 0; i < group.Count; i++)
+        {
+            GameObject go = new GameObject("Group-" + group[i]);
+            go.transform.SetParent(m_UIParent, false);
+            m_UIGroups.Add(group[i], go.transform);
+        }
+    }
+
+    Transform GetUIGroup(string group)
+    {
+        if (!m_UIGroups.ContainsKey(group))
+            Debug.LogError("group is not exist");
+        return m_UIGroups[group];
+    }
+
+    public void OpenUI(string uiName,string group, string luaName)
     {
         GameObject ui = null;
         if (m_UI.TryGetValue(uiName, out ui))
@@ -21,6 +48,10 @@ public class UIManager : MonoBehaviour
         {
             ui = Instantiate(obj) as GameObject;
             m_UI.Add(uiName, ui);
+
+            Transform parent = GetUIGroup(group);
+            ui.transform.SetParent(parent, false);
+            
             UILogic uiLogic = ui.AddComponent<UILogic>();
             uiLogic.Init(luaName);
             uiLogic.OnOpen();
